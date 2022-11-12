44a2eb7a515326e3a146addfece9aeacc135ec68
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-11-12 23:59:08 +0600

    Add files via upload

---
+++ 486A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<a/2<<endl;
+    }
+    else
+    {
+        cout<<-((a/2)+1);
+    }
+}
---
+++ 61A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a,b,c;
+    cin>>a>>b;
+
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]==b[i]){
+            cout<<0;
+        }
+        else{
+            cout<<1;
+        }
+    }
+
+}
