b20a177bd13ffaeba48b4233ef470640c57d9bfb
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-11-12 20:58:41 +0600

    Add files via upload

---
+++ 1748A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    for (int i=0;i<t;i++)
+    {
+        cin>>n;
+        if(n%2==0)
+            cout<<n/2<<endl;
+        else
+            cout<<n/2+1<<endl;
+    }
+}
