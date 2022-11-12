b2e550f931ae7ee3815682545a9ba41a76fafcbf
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-11-12 23:30:53 +0600

    Add files via upload

---
+++ 136A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int j=1;j<=a;j++)
+    {
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]==j)
+            {
+                cout<<i+1<<" ";
+            }
+        }
+    }
+}
