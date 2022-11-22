09e40556cf0da26f415ba51ce49a8e0651fb14e1
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-11-22 20:56:18 +0600

    Add files via upload

---
+++ 472A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n;
+   if(n%2==0)
+   {
+       cout<<n-4<<" 4"<<endl;
+   }
+   else
+   {
+       cout<<n-9<<" 9"<<endl;
+   }
+}
