ce92fd6a87b9ca70046536614d41981bbae26d49
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-11-22 21:54:28 +0600

    Add files via upload

---
+++ 151A.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    cout<<min(min(k*l/nl,c*d),p/np)/n<<endl;
+    return 0;
+}
