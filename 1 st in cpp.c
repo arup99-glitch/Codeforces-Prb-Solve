37bd261647eeb53bdcbe887a4404974d9fca3900
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-12-03 23:57:40 +0600

    Add files via upload

---
+++ 1 st in cpp.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+  public:
+      void Arup(){
+      int roll=30;
+      roll +=5;
+      count << "this rollNo: " << roll
+      }
+};
+int main(){
+Student s;
+s.roll();
+return 0;
+}
