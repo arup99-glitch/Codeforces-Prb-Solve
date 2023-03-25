df2c34f4defcd3bd42585719eabb818759a9c3f1
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2023-03-25 15:54:54 +0600

    Add files via upload

---
+++ 1809B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n,ans;
+        cin>>n;
+        ans=sqrt(n);
+        cout<<ans<<endl;
+    }
+}
